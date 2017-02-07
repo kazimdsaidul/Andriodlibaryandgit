76c2ca5da822ace76d4fd184c61bed545656aafc
kazi Md. Saidul <fardin95@gmail.com>
2017-02-08 00:50:59 +0600

    add my home libaray

---
+++ homelibary/src/main/java/listview/saidul/com/homelibary/MyModualFile.java
@@ -0,0 +1,13 @@
+package listview.saidul.com.homelibary;
+
+/**
+ * Created by kazi on 2/8/17.
+ */
+
+public class MyModualFile {
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
