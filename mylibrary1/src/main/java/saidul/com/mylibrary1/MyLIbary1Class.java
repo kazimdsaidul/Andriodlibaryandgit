77adfbc6b96b95365a0ec89becbffb3708a2aabb
Kazi Md. Saddul <fardin95@gmail.com>
2017-02-08 00:08:59 +0600

    inii

---
+++ mylibrary1/src/main/java/saidul/com/mylibrary1/MyLIbary1Class.java
@@ -0,0 +1,14 @@
+package saidul.com.mylibrary1;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Admin on 2/8/17.
+ */
+
+public class MyLIbary1Class {
+    public static void say(Context context){
+        Toast.makeText(context, "Libay1", Toast.LENGTH_LONG).show();
+    }
+}
