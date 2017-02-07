40f36b37c3513b7238d06daa6f46558a1559a75b
Kazi Md. Saddul <fardin95@gmail.com>
2017-02-08 00:15:50 +0600

    Add libary2

---
+++ mylibrary2/src/main/java/saidul/com/mylibrary2/MyLibary2Class.java
@@ -0,0 +1,14 @@
+package saidul.com.mylibrary2;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Admin on 2/8/17.
+ */
+
+public class MyLibary2Class {
+    public static  void say(Context context){
+        Toast.makeText(context, "hello", Toast.LENGTH_LONG).show();
+    }
+}
